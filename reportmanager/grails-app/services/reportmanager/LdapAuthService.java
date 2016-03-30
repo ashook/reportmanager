0637ba9120908a9c4f09558f20b9f05414762751
Ravi Shah <ravi_b.shah@yahoo.in>
2016-03-30 02:41:14 -0700

    authentication, get summary, basic features

---
+++ reportmanager/grails-app/services/reportmanager/LdapAuthService.java
@@ -0,0 +1,42 @@
+package reportmanager;
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
+public enum LdapAuthService {	
+
+    INSTANCE;
+
+    DirContext ctx = null;
+    
+    public boolean authenticateUser(String ldapUrl, String username, String password, String domains) {
+
+        String[] domainArr = domains.split(",");
+        for (String domain : domainArr) {
+            Hashtable env = new Hashtable();
+            env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+            env.put(Context.PROVIDER_URL, ldapUrl);
+            env.put(Context.SECURITY_AUTHENTICATION, "simple");
+            env.put(Context.SECURITY_PRINCIPAL, domain + "\\" + username);
+            env.put(Context.SECURITY_CREDENTIALS, password);
+            try {
+                ctx = new InitialDirContext(env);
+                return true;
+            } catch (Exception e) {
+            } finally {
+                if (ctx != null) {
+                    try {
+                        ctx.close();
+                    } catch (NamingException ex) {
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+}
